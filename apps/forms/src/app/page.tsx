a96f96794b659b42edee965c1342d77db17fe686
Chris Miller <cmiller@6ps.com>
2025-11-26 02:55:36 -0500

    Simplify dev header banner

--- apps/forms/src/app/page.tsx
+++ apps/forms/src/app/page.tsx
@@ -111,22 +111,6 @@ export default function Home() {
                 Development Mode
               </div>
             </motion.div>
-
-            <motion.div
-              initial={{ opacity: 0, x: 20 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ delay: 0.4, duration: 0.6 }}
-              className="flex items-center gap-2 text-xs font-medium rounded-full bg-gray-50 px-3 py-1 text-gray-600"
-            >
-              <svg className="w-3 h-3 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
-                <path
-                  fillRule="evenodd"
-                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              Secure preview
-            </motion.div>
           </div>
         </div>
       </motion.header>
